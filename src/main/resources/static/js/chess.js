215510877bbd3a32c5f388158f43ae995efdc6a9
Mathias <mathias.boillot@gmail.com>
2020-05-25 20:49:15 +0200

    Saving moves in BDD Display dynamic text for the player's turn

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -60,18 +60,47 @@ $(function () {
         accept: ".figure",
         tolerance: 'intersect',
         over: function () {
-            $(this).css("background-color","green");
+            $(this).css("background-color", "green");
         },
         out: function () {
             $(this).css("background-color", "")
         },
         drop: function (event, ui) {
-            var dropped = ui.draggable;
-            var droppedOn = $(this);
-            $(droppedOn).css("background-color","");
+            let dropped = ui.draggable;
+            let droppedOn = $(this);
+            $(droppedOn).css("background-color", "");
             $(droppedOn).droppable("disable");
             $(dropped).parent().droppable("enable");
-            $(dropped).detach().css({top:0, left: 0}).appendTo(droppedOn);
+            $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+
+            /* CREATE LINK TO UPDATE POSITION OF A PIECE */
+            let URL = $(location).attr('href');
+            URL = URL.split('/');
+            // URL = URL.substring(32);
+            let pawnId = $(droppedOn).children().attr('data-id');
+            let x = $(droppedOn).attr('data-x');
+            let y = $(droppedOn).attr('data-y');
+            URL = '/game/move/' + URL[5] + '/' + pawnId + '/' + x + '/' + y;
+
+            /* REPLACE TEXT FOR THE TURN OF THE PLAYER */
+            let TEXT_TURN = $('#player-turn').html();
+
+
+
+            $.ajax({
+                url: URL, success: function (result) {
+                    console.log("Moves save in bdd");
+
+                    /* REPLACE TEXT FOR THE TURN OF THE PLAYER */
+                    if(TEXT_TURN === 'Black') {
+                        $('#player-turn').text(TEXT_TURN.replace(TEXT_TURN,"White"))
+                    }
+                    else if(TEXT_TURN === 'White') {
+                        $('#player-turn').text(TEXT_TURN.replace(TEXT_TURN,"Black"))
+                    }
+                    /*********/
+                }
+            });
         },
     });
 });
