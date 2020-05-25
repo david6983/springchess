4146c455ee3ea6b85b3f54901a02942083981255
Mathias <mathias.boillot@gmail.com>
2020-05-25 16:40:58 +0200

    DRAG & DROP work !

--- src/main/resources/static/js/chess.js
+++ src/main/resources/static/js/chess.js
@@ -27,16 +27,53 @@ board.find('.figure')
     });*/
 //https://codepen.io/Sulzh/pen/gmmjpQ
 
-board.find('.figure').draggable({ containment: board, revert: 'invalid' });
+// board.find('.figure').draggable({ containment: board, revert: 'invalid' });
+//
+// board.find('.ui-droppable').droppable({
+//     drop: function(ev, ui) {
+//         var dropped = ui.draggable;
+//         var droppedOn = $(this);
+//         $(droppedOn).droppable("disable");
+//         $(dropped).parent().droppable("enable");
+//         // $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
+//     }
+// });
 
-board.find('.figure').droppable({
-    drop: function(ev, ui) {
-        var dropped = ui.draggable;
-        var droppedOn = $(this);
-        $(droppedOn).droppable("disable");
-        $(dropped).parent().droppable("enable");
-        $(dropped).detach().css({top: 0, left: 0}).appendTo(droppedOn);
-    }
+/*
+SOLUTION POUR DRAG & DROP
+ */
+$(function () {
+
+    $('.figure').draggable({
+        containment: board,
+        zIndex: 1,
+        snap: ".cell",
+        revert: function () {
+            if ($(this).hasClass('drag-revert')) {
+                $(this).removeClass('drag-revert');
+                return true;
+            }
+        }
+    });
+
+    $('.cell').droppable({
+        accept: ".figure",
+        tolerance: 'intersect',
+        over: function () {
+            $(this).css("background-color","green");
+        },
+        out: function () {
+            $(this).css("background-color", "")
+        },
+        drop: function (event, ui) {
+            var dropped = ui.draggable;
+            var droppedOn = $(this);
+            $(droppedOn).css("background-color","");
+            $(droppedOn).droppable("disable");
+            $(dropped).parent().droppable("enable");
+            $(dropped).detach().css({top:0, left: 0}).appendTo(droppedOn);
+        },
+    });
 });
 
 board.find('.figure').not('td:empty').droppable("disable");
